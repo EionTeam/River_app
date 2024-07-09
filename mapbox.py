b3eae4b7258482aae50b7f78c31d29d46a4110cf
johnwoodill <johnwoodill@gmail.com>
2024-07-08 20:14:54 -0600

    fixing text

--- mapbox.py
+++ mapbox.py
@@ -127,12 +127,6 @@ with col1:
 with col2:
     rand_b = st.button('Random Location', key='rand')
 
-# Example usage of the buttons
-if go_b:
-    st.write("Go button clicked!")
-if rand_b:
-    st.write("Random Location button clicked!")
-
 
 
 def main(coords):
